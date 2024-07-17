244a1fb27e30584ad624e34911d6e2acfd12d346
spite897 <89777527@qq.com>
2024-07-17 08:50:04 +0800

    Update game.h

--- game.h
+++ game.h
@@ -11,13 +11,13 @@
 #define MAX_TURN 8
 #define SAME_ABLE 0
 #define NAIL_MAX 6
-#define NAIL_MIN 1		//¿ªÇø¼ä
+#define NAIL_MIN 1		//Â¿ÂªÃ‡Ã¸Â¼Ã¤
 
-void Initialize(int* answer , int* input , int * current_turn);
-void Input(int* answer , int* input , int * current_turn);
-int Check(int* answer , int* input , int * current_turn);
-void Next_turn(int* answer , int* input , int * current_turn);
+void initialize(int* answer , int* input , int * current_turn);
+void input(int* answer , int* input , int * current_turn);
+int check(int* answer , int* input , int * current_turn);
+void nextTurn(int* answer , int* input , int * current_turn);
 
-void Test(int* answer , int* input , int * current_turn);
+void test(int* answer , int* input , int * current_turn);
 
 #endif
